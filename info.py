45c3d05ba8370feaae42c4d4317d908834a7392a
Surajsurya95096 <surajroxofficial01@gmail.com>
2025-04-19 08:29:56 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,7 +15,7 @@ def is_enabled(value, default):
 #main variables
 API_ID = int(environ.get('API_ID', '26992030'))
 API_HASH = environ.get('API_HASH', '4da7d71c6bc4512a886e41aca83a5ee3')
-BOT_TOKEN = environ.get('BOT_TOKEN', '8008860383:AAHfw-A_hxEgP8myZVe2_gRdiPiZA1i5RGg')
+BOT_TOKEN = environ.get('BOT_TOKEN', '8119956185:AAGX5Eyr5c3SVSRf9jLWATrKqp40O62Ws2o')
 
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7474015499').split()]
 USERNAME = environ.get('USERNAME', "t.me/Deendayal_dhakad_Contact")
